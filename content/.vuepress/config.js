7797b090d5d1f6c5723f50e5fe5d34fde08b3445
Rodger Jordas <rmjordas@gmail.com>
2019-03-16 03:07:44 +0800

    Update Vuepress configuration

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -3,11 +3,11 @@ module.exports = {
   description: 'A curated list of awesome things related to Vue.js',
   head: [
     ['link', { rel: 'icon', href: `/logo.png` }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon.png` }],
     ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
     ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#fff' }]
